f398f21bffd99e10d50d8f68206472088b6ab49f
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 18:32:34 -0400

    Empezar un Negocio

---
+++ EmpezarUnNegocio.java
@@ -0,0 +1,12 @@
+public class EmpezarUnNegocio{
+	public static void main(String[] args){
+
+	System.out.println("Tomar un prestamo para negocios.");
+	System.out.println("Disenar un buen plan de negocios.");
+	System.out.println("Presentar plan de negocios a socios.");
+	System.out.println("Idear una buena iniciativa de advertisement.");
+	System.out.println("Promocionar el producto, samplear.");
+	System.out.println("Ser consistente en el modelo de negocio.");
+}
+
+}
