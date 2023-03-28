68fdce7bae822a147971c314acbcd74de38db406
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 18:02:37 -0400

    Comprar Pantalon Usado

---
+++ ComprarPantalonUsado.java
@@ -0,0 +1,11 @@
+public class ComprarPantalonUsado{
+	public static void main(String[] args) {
+
+	System.out.println("Entro a la tienda de ropa usada.");
+	System.out.println("Elijo el pantalon que quiero comprar.");
+	System.out.println("Me mido el pantalon que comprare.");
+	System.out.println("Pago el pantalon en caja.");
+	System.out.println("Salgo de la tienda y voy a casa.");
+}
+
+}
