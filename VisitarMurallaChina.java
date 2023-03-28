b630195d212a07d28ca22d70df6cf50537242cfb
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 18:47:55 -0400

    Visitar Muralla China

---
+++ VisitarMurallaChina.java
@@ -0,0 +1,11 @@
+public class VisitarMurallaChina{
+	public static void main(String[] args){
+
+	System.out.println("Tomar un vuelo hacia Beijing.");
+	System.out.println("Viajar en autobus por la autovia.");
+	System.out.println("Visitar la muralla y otros centros historicos.");
+	System.out.println("Tomar muchas fotos.");
+	System.out.println("Regresar a casa.");
+}
+
+}
