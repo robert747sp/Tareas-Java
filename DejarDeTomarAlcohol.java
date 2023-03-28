daf504a169b0e7ec645e8e65de8d058b1deff238
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 18:26:59 -0400

    Dejar de Tomar Alcohol

---
+++ DejarDeTomarAlcohol.java
@@ -0,0 +1,11 @@
+public class DejarDeTomarAlcohol{
+	public static void main(String[] args) {
+	
+	System.out.println("Me inscribo en Alcoholicos Anonimos.");
+	System.out.println("Evito el alcohol a toda costa.");
+	System.out.println("Evito las farras y fiestas donde toman alcohol.");
+	System.out.println("Me congrego a la iglesia mas cercana.");
+	System.out.println("Cambio de vida y de amistades si es necesario.");
+}
+
+}
