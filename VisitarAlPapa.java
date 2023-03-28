f8468fd6b43d537bca7fc65549aad99234173a90
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 17:37:39 -0400

    Visitar Al Papa

---
+++ VisitarAlPapa.java
@@ -0,0 +1,13 @@
+public class VisitarAlPapa{
+	public static void main(String[] args) {
+	
+	System.out.println("Compro un vuelo a Ciudad del Vaticano.");
+	System.out.println("Llego al vaticano.");
+	System.out.println("Pido una reunion con el Papa.");
+	System.out.println("Me reuno con el Papa.");
+	System.out.println("Hablamos de la vida y del amor.");
+	System.out.println("Me despido del Papa.");
+	System.out.println("Vuelvo a casa.");
+}
+
+}
