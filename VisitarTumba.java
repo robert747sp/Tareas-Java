0f68023d45137deef84e7d2677b76defa690ea3a
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 17:28:33 -0400

    Add files via upload

---
+++ VisitarTumba.java
@@ -0,0 +1,10 @@
+public class VisitarTumba{ 
+	public static void main(String[] args) {
+
+	System.out.println("Llego a la tumba del Rey Alfred");
+	System.out.println("Pago la entrada a la tumba");
+	System.out.println("Entro a la tumba del Rey Alfred");
+	System.out.println("Tomo muchas fotos de la tumba");
+}
+
+}
