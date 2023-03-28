1c5d20bb43bbe2597cc9dc6cf73cd1ebf0fe429a
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 19:12:54 -0400

    Hablemos de Obi Wan Kenbi

---
+++ ObiWanKenobi.java
@@ -0,0 +1,11 @@
+public class ObiWanKenobi{
+	public static void main(String[] args) {
+
+	System.out.println("Obi Wan es el Master Jedi.");
+	System.out.println("Obi Wan es el maestro de Luke.");
+	System.out.println("Obi Wan es un buen tipo.");
+	System.out.println("Obi Wan es el final.");
+	System.out.println("Obi Wan es mi amigo.");
+}
+
+}
