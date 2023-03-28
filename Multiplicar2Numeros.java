14b104a3e696c81acfe1c21f3b074c623a5e6539
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 19:22:03 -0400

    Multiplicar

---
+++ Multiplicar2Numeros.java
@@ -0,0 +1,9 @@
+public class Multiplicar2Numeros{
+	public static void main(String[] args) {
+
+	System.out.println("Se toman los dos numeros a multiplicar.");
+	System.out.println("Se multiplica el primero por el segundo.");
+	System.out.println("Se escribe el producto en el espacio correspondiente.");
+}
+
+}
