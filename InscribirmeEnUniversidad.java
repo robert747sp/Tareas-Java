32ae3f209733e503f75b4e1ce9cb7143b89b77f8
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 18:19:35 -0400

    Inscribirme En Universidad

---
+++ InscribirmeEnUniversidad.java
@@ -0,0 +1,11 @@
+public class InscribirmeEnUniversidad{
+	public static void main(String[] args) {
+
+	System.out.println("Llego a la universidad.");
+	System.out.println("Tomo el turno para inscribirme.");
+	System.out.println("Selecciono las Asignaturas del trimestre");
+	System.out.println("Pago el trimestre");
+	System.out.println("Salgo de la universidad y me dirijo a casa");
+}
+
+}
