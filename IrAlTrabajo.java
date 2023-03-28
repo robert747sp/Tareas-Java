87ff74604c341f0f3760642bb77017c13dae186f
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-27 23:32:27 -0400

    Ir Al Trabajo
    Ejercicio 1

---
+++ IrAlTrabajo.java
@@ -0,0 +1,9 @@
+public class IrAlTrabajo{
+	public static void main(String[] args) {
+
+	System.out.println("Me levanto en la mañana");
+	System.out.println("Me cepillo los dientes");
+	System.out.println("Me baño");
+}
+
+}
