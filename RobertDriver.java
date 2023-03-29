ca0b3ca443ed3559bed94f8fc68fc8f252a951cb
Robert Infante <127917935+robert747sp@users.noreply.github.com>
2023-03-29 00:56:59 -0400

    Practica Clase 3

---
+++ RobertDriver.java
@@ -0,0 +1,10 @@
+public class RobertDriver{
+	public static void main(String[] args){
+		String name = "Robert";
+		int edad =29;
+		int cedula = 402-2294782-0;	
+	System.out.println(" Mi nombre es" + name + " Mi edad es" + edad + " Mi cedula es" +cedula);
+
+}
+
+}
