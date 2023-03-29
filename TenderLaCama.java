e4578ef9d5f997563b9e87a52add5f9133c757b4
robert747sp <127917935+robert747sp@users.noreply.github.com>
2023-03-28 20:15:54 -0400

    Tender La Cama

---
+++ TenderLaCama.java
@@ -0,0 +1,13 @@
+public class TenderLaCama{
+	public static void main(String[] arg) {
+	
+	System.out.println("Buscar las sabanas a utilizar en el gavetero.");
+	System.out.println("Quitar las sabanas sucias.");
+	System.out.println("Quitar las fundas de almohadas.");
+	System.out.println("Poner la sabana nueva.");
+	System.out.println("Poner el cubre cama.");
+	System.out.println("Poner las fundas de almohada.");
+
+}
+
+}
