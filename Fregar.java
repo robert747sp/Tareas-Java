e2056a00bf595f305c1ea50fb3438b3ea603ca0e
Robert Infante <127917935+robert747sp@users.noreply.github.com>
2023-03-29 00:08:01 -0400

    Fregar y Vaso de agua, en VisualStudioCode

---
+++ Fregar.java
@@ -0,0 +1,12 @@
+public class Fregar{
+    public static void main(String[] args) {
+
+        System.out.println("Ve a la cocina.");
+        System.out.println("Organiza los trastes.");
+        System.out.println("Toma la esponja, llenala de jabon.");
+        System.out.println("Estriega los trastes, empieza por la cuberteria.");
+        System.out.println("Luego estriega los vasos y platos y por ultimo los calderos.");
+        System.out.println("Enjuaga todo con agua limpia, pon todo en el escurridor secalo con un paño.");
+        System.out.println("Guarda todo en sus lugares correspondientes.");
+    }
+}
---
+++ VasoDeAgua.java
@@ -0,0 +1,11 @@
+public class VasoDeAgua {
+    public static void main(String[] args) {
+
+        System.out.println("Ir a la cocina.");
+        System.out.println("Seleccionar tu vaso preferido.");
+        System.out.println("Ve a la nevera.");
+        System.out.println("Llenalo de agua.");
+        System.out.println("Disfruta tu agua, mantente hidratado.");
+    
+    }
+}
