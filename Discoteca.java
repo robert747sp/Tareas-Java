7f89a9c8d135561de93301774a82daeda2503a4e
Robert Infante <127917935+robert747sp@users.noreply.github.com>
2023-03-29 23:02:25 -0400

    Discoteca wuu

---
+++ Discoteca.java
@@ -0,0 +1,13 @@
+/*Si tiene 18 anos puede entrar, de  lo contrario, no puede entrar */
+
+public class Discoteca{
+	public static void main(string[]	args){
+		int edadMaria = 16;
+
+			if(edadMaria >= 18){
+				System.out.println("Si puede entrar a la disco");
+}else{
+				System.out.println("No puede entrar");
+}//closed else
+	}//closed main
+}//closed class
