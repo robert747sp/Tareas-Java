d6ead36b79eb07d786c526138f368b64da376018
Robert Infante <127917935+robert747sp@users.noreply.github.com>
2023-04-05 18:29:30 -0400

    Hora menu Tarea 3

---
+++ HoraMenu.java
@@ -0,0 +1,15 @@
+public class HoraMenu{
+
+	public static void main(String[] args){
+
+		int horaReloj = 11;
+		if(horaReloj !=11){
+		System.out.println("Menu bebidas: jugo de manzana, Jugo de Chinola, Jugo de piña");
+		}else{
+		System.out.println("Menu comidas: Arroz, Habichuelas, Carne");
+
+}
+
+}
+
+}
